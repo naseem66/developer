8b721cbd0232e481fb760a02e9cca630af7b35f6
khann4 <khann4@AULT-0806E141.global.anz.com>
2019-11-19 11:44:35 +1100

    first commit...

---
+++ src/GitDemoProjec1.java
@@ -0,0 +1,13 @@
+
+public class GitDemoProjec1 {
+	
+	public static void main() {
+		String hungry="yes";
+		if(hungry=="yes") 
+			System.out.println("eat something");
+		else {
+			System.out.println("do work...");
+		}
+	}
+
+}
