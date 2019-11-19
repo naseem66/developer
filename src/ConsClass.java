5b0dba8fb28be41e932bfe080036c1ddbb84e63f
khann4 <khann4@AULT-0806E141.global.anz.com>
2019-11-19 15:16:31 +1100

    i have committed new code in new branch

---
+++ src/ConsClass.java
@@ -0,0 +1,17 @@
+
+public class ConsClass {
+	public static void main(String args[]) {
+		String data="legitimate";
+	if(data=="legitimate"||data=="Legitimate"||data=="Legit") 
+		System.out.println("there is hunger strike"+data);
+	
+		
+	else {
+		
+	 System.out.println("not legitimate");
+		
+	}
+	
+	}
+
+}
