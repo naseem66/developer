a9de7c4e074a1e009cd1ca45571f6ed85fc6a182
khann4 <khann4@AULT-0806E141.global.anz.com>
2019-11-19 12:08:14 +1100

    committing test1 to branchx

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+	
+	public static void main() {
+		String hungry="yes";
+		if(hungry=="yes") 
+			System.out.println("eat something");
+		else {
+			System.out.println("do work...");
+		}
+	}
+
+}
