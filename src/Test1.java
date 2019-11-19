478f5cf527889e13ede469cb034727eddc31ab92
khann4 <khann4@AULT-0806E141.global.anz.com>
2019-11-19 13:36:21 +1100

    added additional check

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
 	
 	public static void main() {
 		String hungry="yes";
-		if(hungry=="yes") 
+		if(hungry=="yes"|| hungry=="Y"||hungry=="y") 
 			System.out.println("eat something");
 		else {
 			System.out.println("do work...");
